9991125c76d444b8675dc990c26c15386058c8a1
LeHaine <colt.daily@gmail.com>
2019-01-25 21:34:20 -0500

    add resize event listener.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 import Client from "./client/phaser/Client";
 
-window.game = new Client();
+window.onload = () => {
+    window.game = new Client();
+    resize();
+    window.addEventListener("resize", resize, false);
+};
+
+const resize = () => {
+    let client = window.game;
+    let canvas = document.querySelector("canvas");
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let wratio = width / height;
+    let ratio = client.config.width / client.config.height;
+    if (wratio < ratio) {
+        canvas.style.width = width + "px";
+        canvas.style.height = width / ratio + "px";
+    } else {
+        canvas.style.width = height * ratio + "px";
+        canvas.style.height = height + "px";
+    }
+};
