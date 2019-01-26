c1b431152137c8e4322906b1892847f38ee24e42
LeHaine <colt.daily@gmail.com>
2019-01-25 21:50:18 -0500

    update config width and height.

--- src/client/phaser/Client.js
+++ src/client/phaser/Client.js
@@ -3,8 +3,8 @@ import GameScene from "./scenes/GameScene";
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 800,
     parent: "content",
     backgroundColor: "33ccff",
     scene: [GameScene]
